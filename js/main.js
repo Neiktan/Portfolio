2f4595ecaa265afbd3b3fac3255effe1ab1cbdfc
Neiktan <wichoalguiar@gmail.com>
2023-06-11 18:53:19 -0500

    css's

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 var typed = new Typed(".text", {
-    strings:["fronted Developer" , "Web Developer"],
+    strings:["Fronted Developer" , "Web Developer"],
     typeSpeed: 100,
     backSpeed: 100,
     backDelay: 1000,
