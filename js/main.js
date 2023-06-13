e485ea1d70d4fc36449fb04d12fbaf0d29511047
Neiktan <wichoalguiar@gmail.com>
2023-06-12 20:44:53 -0500

    act date 6/12

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 var typed = new Typed(".text", {
-    strings:["Fronted Developer" , "Web Developer"],
+    strings:["Fronted Developer" , "Web Developer" , "Cybersecurity researcher"],
     typeSpeed: 100,
     backSpeed: 100,
     backDelay: 1000,
